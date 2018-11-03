94bfadb6ed950dab02d5fc01e6647ebd05f93bec
Kanav Anand <anandkanav92@gmail.com>
2018-11-03 16:00:20 +0100

    adds api services server.py

---
+++ server/main.py
@@ -0,0 +1,70 @@
+from flask import Flask,request
+from waitress import serve
+from algoliasearch import algoliasearch
+import base64
+from PIL import Image
+import random
+import json
+from sample import getCaption
+from build_vocab import Vocabulary
+
+app = Flask(__name__)
+client = algoliasearch.Client("-", '-')
+index = client.init_index('random')
+index.set_settings({
+  'searchableAttributes': [
+    'caption'
+  ]
+})
+
+
+
+@app.route('/classify', methods=['GET', 'POST'])
+def putResultsInAlgolia():
+    # print("Request received!")
+    # url = "facebook.com"
+    # caption = "delete that shit"
+    # x = {"url": url, "caption": caption}
+    # index.addObject(x)
+    # return caption
+    content = request.get_json(silent=True)
+
+    # Got image encoded in base 64, need to convert it to png
+    blah = content['base64image']
+    blah = blah.replace("data:image/png;base64,", "")
+    blah = blah.replace(" ", "+")
+    if content:
+        # converting base64 image to png
+        with open("imageToSave.png", "wb") as fh:
+            fh.write(base64.decodebytes(bytes(blah, 'utf-8')))
+        im = Image.open("imageToSave.png")
+        s = str(random.randint(1, 100000)) + ".png"
+        im.save("collection/" + s)
+
+        #rgb_im = im.convert('RGB')
+        #rgb_im.save('imageToSave.jpg')
+
+        result = getCaption("collection/" + s)
+        if result != None:
+            index.addObject(result)
+            print("PREDICTION:", result)
+            return json.dumps({'status': 'OK', 'caption': result.caption})
+        return json.dumps({"status": "ERROR"})
+    else:
+        return json.dumps({"status": "ERROR"})
+
+
+@app.route('/getCaptions', methods=['GET'])
+def getResultsFromAlgolia():
+    content = request.args.get('query')
+    result = index.search(content,
+    {"attributesToRetrieve": "url,caption",'restrictSearchableAttributes': [
+    'caption'
+     ]})
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    serve(app,host='0.0.0.0', port=5002)
+    app.run()
+
